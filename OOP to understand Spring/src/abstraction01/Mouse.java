7a45cf8a7684870dfdff224eb9548a43fdd75499
inmoG <inmoy@naver.com>
2022-11-01 17:24:53 +0900

    create abstraction package

--- OOP to understand Spring/src/abstraction01/Mouse.java
+++ OOP to understand Spring/src/abstraction01/Mouse.java
@@ -1,2 +1,11 @@
-package abstraction01;public class Mouse {
+package abstraction01;
+
+public class Mouse {
+    public String name;
+    public int age;
+    public int countOfTail;
+
+    public void sing(){
+        System.out.println(name + " 찍찍 !!");
+    }
 }
