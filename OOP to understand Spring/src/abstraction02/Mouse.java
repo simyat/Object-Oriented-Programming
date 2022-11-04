74d71da052fce779c3f2f6fa734420a89c158085
inmoG <inmoy@naver.com>
2022-11-04 23:46:54 +0900

    class, static variable

---
+++ OOP to understand Spring/src/abstraction02/Mouse.java
@@ -0,0 +1,13 @@
+package abstraction02;
+
+public class Mouse {
+    public String name;
+    public int age;
+    public static int countOfTail = 1;
+
+
+    public void sing(){
+        System.out.println(name + " 찍찍찍 !!");
+    }
+
+}
